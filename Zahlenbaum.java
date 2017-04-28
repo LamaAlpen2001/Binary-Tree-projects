2cb3a2ff2b2e630f15bcf649f5c1afb0cb434e4d
ErikNathrath <erik@nathrath.com>
2017-04-28 12:15:09 +0200

    Add files via upload
    this is unbelivable! it is nothing short of a revolution!

--- Zahlenbaum.java
+++ Zahlenbaum.java
@@ -1,6 +1,6 @@
 public class Zahlenbaum
 {
-    private BinaryTree zahlenbaum;
+    private BinaryTree<Integer> z1,z2,z3,z4,z5,z6,z7;
 
     public Zahlenbaum()
     {
@@ -10,6 +10,21 @@ public class Zahlenbaum
      BinaryTree<Integer> z4 = new BinaryTree(11, z3, null);
      BinaryTree<Integer> z5 = new BinaryTree(9, z2, z4);
      BinaryTree<Integer> z6 = new BinaryTree(3);
-     BinaryTree<Integer> zahlenbaum = new BinaryTree(5, z6, z5);
+     BinaryTree<Integer> z7 = new BinaryTree(5, z6, z5);
+    }
+    public void preOrderStart(){
+        System.out.println("");
+        preorder(z7);
+    }
+    public void preorder(BinaryTree<Integer> b){
+        if(!b.isEmpty()){
+            System.out.println(b.getContent()+" ");
+        }
+        if(!b.getLeftTree().isEmpty()){
+            preorder(b.getLeftTree());
+        }
+        if(!b.getRightTree().isEmpty()){
+            preorder(b.getRightTree());
+        }
     }
 }
